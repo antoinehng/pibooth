7a37525978a84b3c80206eac113ce04327d41952
antoinehng <antoine@macbookpro.home>
2018-09-23 17:50:53 +0200

    update dev script

--- dev.py
+++ dev.py
@@ -1,6 +1,11 @@
 # -*- coding: utf8 -*-
 
 from pibooth.camera import Camera
+from pibooth.button import Button
 
 c = Camera()
 c.take_photo()
+
+b = Button(18)
+print("wait for button press")
+b.on_press(c.take_photo)
