8686125dea5d4bda6f9784470b98d9b828f646a9
antoinehng <antoine@macbookpro.home>
2018-09-23 18:53:22 +0200

    try event with debouncing

--- dev.py
+++ dev.py
@@ -1,14 +1,9 @@
 # -*- coding: utf8 -*-
 
-import RPi.GPIO as GPIO  
 
 from pibooth.camera import Camera
 from pibooth.button import Button
-  
-try:  
-    c = Camera()
-    b = Button(18)
-    b.on_press(c.take_photo)
 
-finally:  
-    GPIO.cleanup() # this ensures a clean exit  
+c = Camera()
+b = Button(18)
+b.on_press(c.take_photo)
--- pibooth/button.py
+++ pibooth/button.py
@@ -18,18 +18,12 @@ class Button(object):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    @threaded
     def on_press(self, function):
         """Execute function on button press
 
         :param function: The function to execute on button press
         :type function: function
         """
-        while True:
-            print("wait for button press")
-            GPIO.wait_for_edge(self.pin, GPIO.FALLING) 
-            print("button pressed")
-            print(str(GPIO.input(self.pin)))
-            function()
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=func, bouncetime=200)
 
 
