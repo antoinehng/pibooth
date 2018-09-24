498fac6a2b94d6e9510a0e7aa2533569176bd326
antoinehng <antoine@macbookpro.home>
2018-09-24 21:27:09 +0200

    set a simple printer class
    not tested, don't have the thermal printer yet

---
+++ pibooth/printer.py
@@ -0,0 +1,28 @@
+# -*- coding: utf8 -*-
+
+from PIL import Image
+from Adafruit_Thermal import *
+
+
+class Printer(object):
+    """This class represents the printer"""
+
+    def __init__(self, device, baud, timeout):
+        """Printer initialization
+        
+        :param device: Device path
+        :type device: str
+        :param baud: Baud rate
+        :type baud: int
+        :param timeout: Timeout in seconds
+        :type timeout: int
+        """
+        self.device = Adafruit_Thermal(device, baud, timeout=timeout)
+
+    def print_image(self, image_file_path):
+        """Print Image
+
+        :param image_file_path: Image file path
+        :type image_file_path: str
+        """
+        self.device.printImage(Image.open(image_file_path), True)
