5cdd34118af58a623c3348a4e90a68ad689c6eca
antoinehng <antoine@macbookpro.home>
2018-09-30 18:45:51 +0200

    more detailed print

--- pibooth/booth.py
+++ pibooth/booth.py
@@ -31,6 +31,4 @@ class Booth(object):
         while True:
             if not self.image_path_queue.empty():
                 image_path = self.image_path_queue.get()
-                print(image_path)
                 self.printer.print_image(image_path)
-
--- pibooth/printer.py
+++ pibooth/printer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 
 from __future__ import print_function
+import os
 
 from PIL import Image
 from Adafruit_Thermal import *
@@ -40,14 +41,62 @@ class Printer(object):
         :param image_file_path: Image file path
         :type image_file_path: str
         """
+        # print logo
+        self.device.feed(1)
+        self.device.printImage(Image.open("/home/pi/pibooth/config/logo.png"), True)
+        self.device.feed(1)
 
+        self.device.justify('C')
+
+        self.device.doubleHeightOn()
+        self.device.print("HALO MAUD + YOLANDE BASHING")
+
+        self.device.doubleHeightOff()
+        self.device.print("2018-11-12 21:08:44") # time
+
+        self.device.feed(1)
+
+        # print picture
         image_for_print_path = image_file_path+".print"
-        image_for_print = Image.open(image_file_path)
-        image_for_print = image_for_print.transpose(Image.ROTATE_90)
-        image_for_print.thumbnail(self.print_size, Image.ANTIALIAS)
-        image_for_print.save(image_for_print_path, "JPEG")
-
+        image_for_print = Image.open(image_file_path) # create proxy image for print
+        image_for_print = image_for_print.transpose(Image.ROTATE_90) # rotate image
+        image_for_print.thumbnail(self.print_size, Image.ANTIALIAS) # resize
+        image_for_print.save(image_for_print_path, "JPEG") # save
         self.device.printImage(Image.open(image_for_print_path), True)
         self.device.feed(1)
-        self.device.print("http://bruitblanc.xzy")
+
+        # print text
+        self.device.print("LA CAVE AUX POETES, ROUBAIX")
         self.device.feed(3)
+
+        # line
+        self.device.strikeOn()
+        self.device.print("     ")
+        self.device.strikeOff()
+
+        self.device.feed(1)
+        self.device.boldOn()
+        self.device.print("partagez votre")
+        self.device.print("photo avec le code")
+        self.device.boldOff()
+        self.device.doubleHeightOn()
+        self.device.print("C0D3")
+        self.device.doubleHeightOff()
+        self.device.boldOn()
+        self.device.print("sur")
+        self.device.boldOff()
+        self.device.print("shootomatic.net")
+        self.device.feed(1)
+        
+        # line
+        self.device.strikeOn()
+        self.device.print("     ")
+        self.device.strikeOff()
+
+
+
+        # space to detach 
+        self.device.feed(3)
+
+        # delete proxy image used for print
+        os.remove(image_for_print_path)
